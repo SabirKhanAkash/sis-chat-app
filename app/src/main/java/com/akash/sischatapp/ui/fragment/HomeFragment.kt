7b8d4499f7df3f3126b8883d2b4d565cd8823238
Sabir Khan Akash <sabir.akash.cs@gmail.com>
2023-07-31 00:58:37 +0600

    reformatted code & optimized imports

--- app/src/main/java/com/akash/sischatapp/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/akash/sischatapp/ui/fragment/HomeFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.akash.sischatapp.R
 import com.akash.sischatapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
